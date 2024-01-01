900f0f89a5581507b29ac19cbc3578ea7dcf72b9
Jayanth <jayanth.bagare@gmail.com>
2024-01-01 13:27:55 +0530

    Populate Grid with base values

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ windowHeight = 800
 
 rows,cols = (10,10)
 choices = ['Red','Green','Blue']
-# currarr = [[] * cols] * rows
-# newarr = [[] * cols] * rows
 currarr = np.zeros(shape=(rows,cols))
 newarr = np.zeros(shape=(rows,cols))
 
 #Define the initial conditions
 vacant = 0.2 # 10% of the grid id vacant
 sim_threshold = 0.3 #(1-Tau) the number of neighbours equal and below warrants a move
-pop_split = 'E' #Equal population split, must implement further
+pop_split = 1 # 1:1 Equal population split, must implement further
 
 def init_grid(currarr,newarr,rows,cols):
     currarr = np.zeros(shape=(rows,cols))
@@ -41,5 +39,19 @@ def set_vacant(currarr,newarr,vacant,rows,cols):
     newarr = currarr
     return currarr,newarr
 
+def populate_grid(currarr,newarr):
+    for j in range(cols):
+        for i in range(rows):
+            if(currarr[i][j] != 5):
+                r = random.random()
+                if(r >= 0.5):
+                    currarr[i][j] = 1
+                else:
+                    currarr[i][j] = 2
+    newarr = currarr
+    return currarr,newarr
+
 currarr,newarr = init_grid(currarr,newarr,rows,cols)
 currarr,newarr = set_vacant(currarr,newarr,vacant,rows,cols)
+currarr,newarr = populate_grid(currarr,newarr)
+print(currarr)
