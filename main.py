0c81203c7e464ae8b0ff944c7745c6934d988bbf
Jayanth <jayanth.bagare@gmail.com>
2024-01-01 15:07:45 +0530

    simulation is running, stillt o verify why the last 2 columns are empty

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import random
 import numpy as np
 from pandas import *
@@ -9,13 +10,16 @@ windowWidth = 800
 windowHeight = 800
 
 rows,cols = (10,10)
-choices = ['Red','Green','Blue']
+cell_size = int(windowWidth/cols)
+
+total_movements = 0
+
 currarr = np.zeros(shape=(rows,cols))
 newarr = np.zeros(shape=(rows,cols))
 
 #Define the initial conditions
 vacant = 0.2 # 10% of the grid id vacant
-sim_threshold = 0.3 #(1-Tau) the number of neighbours equal and below warrants a move
+sim_threshold = 0.5 #(1-Tau) the number of neighbours equal and below warrants a move
 pop_split = 1 # 1:1 Equal population split, must implement further
 
 def init_grid(currarr,newarr,rows,cols):
@@ -51,7 +55,78 @@ def populate_grid(currarr,newarr):
     newarr = currarr
     return currarr,newarr
 
+def segregate(currarr,newarr,sim_threshold,total_movements):
+    for i in range(rows):
+        for j in range(cols):
+            if(j+1<cols):
+                east = 1 if currarr[i][j] == currarr[i][j+1] else 0
+            else:
+                east = 0
+
+            if(i-1>=0 and j+1<cols):
+                neast = 1 if currarr[i][j] == currarr[i-1][j+1] else 0
+            else:
+                neast = 0
+
+            if(i-1>=0):
+                north = 1 if currarr[i][j] == currarr[i-1][j] else 0
+            else:
+                north = 0
+            
+            if(i-1 >=0 and j-1>=0):
+                nwest = 1 if currarr[i][j] == currarr[i-1][j-1] else 0
+            else:
+                nwest = 0
+            
+            if(j-1>=0):
+                west = 1 if currarr[i][j] == currarr[i][j-1] else 0
+            else:
+                west = 0
+            
+            if(i+1 < rows and j-1>=0):
+                swest = 1 if currarr[i][j] == currarr[i+1][j-1] else 0
+            else:
+                swest = 0
+            
+            if(i+1 < rows):
+                south = 1 if currarr[i][j] == currarr[i+1][j] else 0
+            else:
+                south = 0
+            
+            if(i+1 < rows and j+1 < cols):
+                seast = 1 if currarr[i][j] == currarr[i+1][j+1] else 0
+            else:
+                seast = 0
+
+            total_count = east + neast + north + nwest + west + swest + south + seast
+
+            if(sim_threshold >= (total_count/8)):
+                #get the first possible vacant location
+                vacant_slots = np.argwhere(newarr == 5)[0]
+                newarr[vacant_slots[0]][vacant_slots[1]] = currarr[i][j]
+                newarr[i][j] = 5
+                total_movements += 1
+    currarr = newarr
+    return currarr,newarr,total_movements
+
 currarr,newarr = init_grid(currarr,newarr,rows,cols)
 currarr,newarr = set_vacant(currarr,newarr,vacant,rows,cols)
 currarr,newarr = populate_grid(currarr,newarr)
-print(currarr)
+
+init_window(windowWidth,windowHeight,"Schelling Segregation Simulation")
+while not window_should_close():
+    begin_drawing()
+    currarr,newarr,total_movements = segregate(currarr,newarr,sim_threshold,total_movements)
+    for i in range(rows):
+        for j in range(cols):
+            draw_rectangle_lines(i*cell_size,j*cell_size,cell_size,cell_size,VIOLET)
+            if(newarr[i][j] == 1):
+                draw_circle(int(i*cell_size + cell_size/2),int(j*cell_size + cell_size/2),cell_size/4,YELLOW)
+            if(newarr[i][j] == 2):
+                draw_circle(int(i*cell_size + cell_size/2),int(j*cell_size + cell_size/2),cell_size/4,GREEN)
+    if(total_movements == 0):
+        end_drawing()
+    else:
+        wait_time(1)
+    end_drawing()
+close_window()
