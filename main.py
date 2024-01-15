aaa3be92e3a2c79aefd8a622789e2ea6a99857de
jayanthbagare <jayanth.bagare@gmail.com>
2024-01-15 16:59:45 +0530

    Final Commit on Schelling

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyray import *
 windowWidth = 800
 windowHeight = 800
 
-rows,cols = (8,8)
+rows,cols = (112,112)
 cell_size = int(windowWidth/cols)
 
 total_movements = 0
@@ -18,9 +18,9 @@ currarr = np.zeros(shape=(rows,cols))
 newarr = np.zeros(shape=(rows,cols))
 
 #Define the initial conditions
-vacant = 0.3 # 10% of the grid id vacant
-sim_threshold = 0.3 #(1-Tau) the number of neighbours equal and below warrants a move
-pop_split = 1 # 1:1 Equal population split, must implement further
+vacant = 0.008 # 10% of the grid id vacant
+sim_threshold = 0.4 #(1-Tau) the number of neighbours equal and below warrants a move
+pop_split = 0.5 # 1:1 Equal population split, must implement further
 
 def init_grid(currarr,newarr,rows,cols):
     currarr = np.zeros(shape=(rows,cols))
@@ -48,10 +48,13 @@ def populate_grid(currarr,newarr):
         for i in range(rows):
             if(currarr[i][j] != 5):
                 r = random.random()
-                if(r > 0.5):
-                    currarr[i][j] = 1
+                if(r <= vacant):
+                    currarr[i][j] = 5
                 else:
-                    currarr[i][j] = 2
+                    if(random.random() <=0.5):
+                        currarr[i][j] = 1
+                    else:
+                        currarr[i][j] = 2
     newarr = currarr
     return currarr,newarr
 
@@ -99,28 +102,24 @@ def segregate(currarr,newarr,sim_threshold,total_movements):
                 seast = 0
 
             total_count = east + neast + north + nwest + west + swest + south + seast
-            if((total_count/8) < sim_threshold):
+            if(total_count/8 < sim_threshold):
                 #get the first possible vacant location
                 vacant_slots = np.argwhere(newarr == 5)[0]
-                # vacant_slots[random.randint(0,len(vacant_slots))]
-                print(vacant_slots[0])
                 newarr[vacant_slots[0]][vacant_slots[1]] = currarr[i][j]
                 newarr[i][j] = 5
                 total_movements += 1
     currarr = newarr
     return currarr,newarr,total_movements
 
-currarr,newarr = init_grid(currarr,newarr,rows,cols)
-currarr,newarr = set_vacant(currarr,newarr,vacant,rows,cols)
-currarr,newarr = populate_grid(currarr,newarr)
-currarr,newarr,total_movements = segregate(currarr,newarr,sim_threshold,total_movements)
-
 init_window(windowWidth,windowHeight,"Schelling Segregation Simulation")
+currarr,newarr = init_grid(currarr,newarr,rows,cols)
+# currarr,newarr = set_vacant(currarr,newarr,vacant,rows,cols)
+currarr,newarr = populate_grid(currarr,newarr)
+
 while not window_should_close():
     begin_drawing()
     total_movements = 0
-    
-    print(total_movements)
+    currarr,newarr,total_movements = segregate(currarr,newarr,sim_threshold,total_movements)
     for i in range(rows):
         for j in range(cols):
             draw_rectangle_lines(i*cell_size,j*cell_size,cell_size,cell_size,VIOLET)
@@ -128,6 +127,6 @@ while not window_should_close():
                 draw_circle(int(i*cell_size + cell_size/2),int(j*cell_size + cell_size/2),cell_size/4,YELLOW)
             if(currarr[i][j] == 2):
                 draw_circle(int(i*cell_size + cell_size/2),int(j*cell_size + cell_size/2),cell_size/4,GREEN)
-    wait_time(1)
+    # wait_time(0.2)
     end_drawing()
 close_window()
